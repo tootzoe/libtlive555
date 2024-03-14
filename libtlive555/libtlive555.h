1604873a3e54a814deb9553674ec1353ae413cf6
toot_hzf <toot@tootzoe.com>
2024-03-14 23:53:50 +0700

    Initial commit....

--- libtlive555/libtlive555.h
+++ libtlive555/libtlive555.h
@@ -9,16 +9,16 @@
 //
 //
 
-
-
-#import <Foundation/Foundation.h>
-
-//! Project version number for libtlive555.
-FOUNDATION_EXPORT double libtlive555VersionNumber;
-
-//! Project version string for libtlive555.
-FOUNDATION_EXPORT const unsigned char libtlive555VersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <libtlive555/PublicHeader.h>
-
-
+//
+//
+//#import <Foundation/Foundation.h>
+//
+////! Project version number for libtlive555.
+//FOUNDATION_EXPORT double libtlive555VersionNumber;
+//
+////! Project version string for libtlive555.
+//FOUNDATION_EXPORT const unsigned char libtlive555VersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <libtlive555/PublicHeader.h>
+//
+//
